1d5039e9ed27d679839f870b8bdc0ad40820d66a
Sasini Munasinghe <167131101+sasinikm@users.noreply.github.com>
2024-05-23 18:24:52 -0400

    Added SQL code for homework-4

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -17,7 +17,9 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
-
+SELECT 
+product_name || ', ' || COALESCE(product_size,'')|| ' (' || COALESCE(product_qty_type,'unit') || ')'
+FROM product;
 
 
 --Windowed Functions
@@ -30,16 +32,31 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+--DENSE_RANK Approach
+SELECT *,
+DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY market_date ASC) as customer_visits		
+FROM customer_purchases;
+
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+SELECT *
+FROM
+	(
+		SELECT *,
+		DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY market_date DESC) as customer_visits		
+		FROM customer_purchases
+	) x
+WHERE x.customer_visits = 1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
-
+SELECT *
+,COUNT(product_id) OVER (PARTITION BY customer_id, product_id) as product_purchases
+FROM customer_purchases;
 
 
 -- String manipulations
@@ -54,10 +71,16 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
+SELECT *
+, LTRIM(SUBSTR(product_name, NULLIF(INSTR(product_name, '-'),0)),'- ') as description
+FROM product;
 
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
+SELECT *
+,LTRIM(SUBSTR(product_name, NULLIF(INSTR(product_name, '-'),0)),'- ') as description
+FROM product
+WHERE product_size REGEXP '(.*[0-9].*$)';
 
 
 -- UNION
@@ -71,5 +94,28 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 with a UNION binding them. */
 
 
+SELECT market_date,sales,'Lowest' as [highest/lowest]
+FROM
+	(	SELECT market_date
+		,SUM(quantity*cost_to_customer_per_qty) as sales
+		,RANK() OVER (order by SUM(quantity*cost_to_customer_per_qty) ASC) as [sales_asc]
+		FROM customer_purchases
+		GROUP BY market_date
+	) x
+		
+WHERE x.[sales_asc] = 1
+
+UNION 
+
+SELECT market_date,sales,'Highest' as [highest/lowest]
+FROM
+	(	SELECT market_date
+		,SUM(quantity*cost_to_customer_per_qty) as sales
+		,RANK() OVER (order by SUM(quantity*cost_to_customer_per_qty) DESC) as [sales_desc]
+		FROM customer_purchases
+		GROUP BY market_date
+	) x
+	
+WHERE x.[sales_desc] = 1;
 
 
