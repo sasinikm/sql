37acc70946a5b78e47988bb8a8f097d57ba74d90
Sasini Munasinghe <167131101+sasinikm@users.noreply.github.com>
2024-05-25 13:44:21 -0400

    Added SQL queries to homework-5 and answers to homework-6

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -9,7 +9,18 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-
+SELECT v.vendor_name,p.product_name,vi.income
+FROM
+	(	SELECT vendor_id, product_id,Sum(original_price*5) as income
+		FROM
+	((	SELECT distinct vendor_id , product_id,original_price
+		FROM vendor_inventory )
+	CROSS JOIN 
+	(	SELECT Customer_id
+		FROM customer))
+GROUP BY vendor_id, product_id) AS vi
+INNER JOIN product p on vi.product_id = p.product_id
+INNER JOIN vendor v on vi.vendor_id=v.vendor_id;
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -17,18 +28,27 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
+CREATE TABLE IF NOT EXISTS product_units AS
+
+	SELECT * , CURRENT_TIMESTAMP AS snapshot_timestamp
+	FROM product
+	WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+INSERT INTO product_units
+VALUES('24','Apple Pie','8"','3','unit',CURRENT_TIMESTAMP);
 
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
-
+DELETE FROM product_units
+WHERE product_id = 7;
 
 
 -- UPDATE
@@ -48,4 +68,18 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
-
+UPDATE product_units
+SET current_quantity =
+	(	SELECT COALESCE(y.quantity, 0)
+		FROM
+			(
+			 SELECT pu.product_id, x.quantity  FROM product_units pu 
+			 LEFT JOIN 
+				(SELECT max(market_date) as m_date, quantity, product_id
+			FROM vendor_inventory
+			GROUP BY product_id
+			) x
+			ON pu.product_id=x.product_id 
+		) y
+		WHERE product_units.product_id = y.product_id
+		);
--- 03_homework/homework_6.md
+++ 03_homework/homework_6.md
@@ -6,3 +6,9 @@
 <br>
 
 **Write**: Reflect on your previous work and how you would adjust to include ethics and inequity components. Total length should be a few paragraphs, no more than one page.
+
+SQL queries mainly focus on extracting and manipulating data efficiently. However, ethical and inequity components are crucial for a responsible data analysis. 
+A fundamental principle involves protecting the privacy and confidentiality of sensitive data. Whenever possible, anonymizing such data before analysis ensures compliance with data privacy regulations. Best practices dictate analyzing how various groups are affected by the data. It's crucial to assess and potentially restructure queries to promote inclusivity within the analysis.
+The initial step towards ethical and inequity query writing involves source review. This entails verifying whether the data was collected fairly and representatively and identifying any biases within the data collection process. Unequal data distribution can be identified using aggregation in SQL. If significant skewness exists, filtering out outliers may help to reduce the distribution's skewness. Additionally, using median and percentile instead of average function reduces the influence of outliers.
+When using SQL for reporting purposes, use of filtered views by demographic factors and use of aggregated data, mitigate the bias and presents useful insights. Proper documentation of data sources and data transformations will give the users an understanding of the bias within data.
+By adhering to the above ethics and inequity components SQL queries promote fairness and mitigate bias. This helps in data-driven decision making that benefits everyone.
